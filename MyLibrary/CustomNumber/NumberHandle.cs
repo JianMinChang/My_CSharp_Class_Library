9434ac22c77a704661e1b92b5d3105d225370ee2
JianMinChang <jackchang0721@gmail.com>
2015-01-06 21:07:50 +0800

    Jack-20150106-add Number Handle

---
+++ MyLibrary/CustomNumber/NumberHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.CustomNumber
+{
+    public class NumberHandle
+    {
+        /// <summary>
+        /// 數字轉字串並缺項補0
+        /// </summary>
+        /// <param name="num">要補0的數字</param>
+        /// <param name="Digits">總共有幾位數</param>
+        /// <returns>已補上不足項的數字字串</returns>
+        public static string intFormat(int num, int Digits)
+        {
+            string snum = num.ToString();
+            return snum.PadLeft(Digits, '0');
+        }
+
+    }
+}
