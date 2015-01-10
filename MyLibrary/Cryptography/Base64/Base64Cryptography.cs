02fe9518e9298388534666363a2d22df34d9e7a0
JianMinChang <jackchang0721@gmail.com>
2015-01-10 22:58:27 +0800

    Jack-20150110-Add Base64Cryptography

---
+++ MyLibrary/Cryptography/Base64/Base64Cryptography.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Cryptography.Base64
+{
+    public static class Base64Cryptography
+    {
+        /// <summary>
+        /// Base64 加密
+        /// </summary>
+        /// <param name="str">須加密的字串</param>
+        /// <returns>加密後字串</returns>
+        public static string Encrypt(string str)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// Base64 解密
+        /// </summary>
+        /// <param name="str">須解密的字串</param>
+        /// <returns>解密後的字串</returns>
+        public static string Decrypt(string str)
+        {
+            return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(str));
+        }
+    }
+}
