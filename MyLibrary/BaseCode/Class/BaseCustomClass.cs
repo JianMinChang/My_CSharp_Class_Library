0b2e19cd29941f58cfa8e8b28ee9cad910a2545d
JianMinChang <jackchang0721@gmail.com>
2015-01-19 20:21:59 +0800

    Jack-20140119-Add BaseClass Code

---
+++ MyLibrary/BaseCode/Class/BaseCustomClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.BaseCode.Class
+{
+    public class BaseCustomClass
+    {
+        public bool IsSuccess { protected set; get; }
+
+        public string ErrorMessage { protected set; get; }
+
+        protected virtual void Reset()
+        {
+            this.IsSuccess = false;
+            this.ErrorMessage = string.Empty;
+        }
+    }
+}
