e9995e8cc842fe4256923b857c42e68af1cf5329
JianMinChang <jackchang0721@gmail.com>
2015-01-24 23:36:46 +0800

    Jack-20150124-Add IBaseRepository Interface

---
+++ MyLibrary/BaseCode/InterFace/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MyLibrary.BaseCode.InterFace
+{
+
+    public interface IBaseRepository
+    {
+        bool IsSuccess { get; }
+        string ErrorMessage { get; }
+
+    }
+}
