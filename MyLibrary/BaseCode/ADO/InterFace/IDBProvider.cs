47aa79d020aa83de46116257a88ecd64a22f8533
JianMinChang <jackchang0721@gmail.com>
2015-01-25 16:26:18 +0800

    Jack-20150125-Add Interface IDBProvider.cs

---
+++ MyLibrary/BaseCode/ADO/InterFace/IDBProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MyLibrary.BaseCode.ADO.InterFace
+{
+    public interface IDBProvider
+    {
+        bool IsSuccess { get; }
+        string ErrorMessage { get; }
+
+
+        CommandType CommandType { set; }
+
+        int ConnectionTimeout { set; }
+
+        string DBConnectionString { set; }
+
+        DataTable ListSQL(string SQLcommand, IList<IDataParameter> Parms);
+
+        DataTable ListSQL(string SQLcommand);
+
+        object ValueListSQL(string SQLcommand, IList<IDataParameter> Parms);
+
+        object ValueListSQL(string SQLcommand);
+
+        object ExecSQL(string SQLcommand, IList<IDataParameter> Parms);
+
+        object ExecSQL(string SQLcommand);
+
+    }
+}
