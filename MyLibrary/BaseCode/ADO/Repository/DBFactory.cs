165ea6d1928a9bb5776af1afe96faf8439a0a00b
JianMinChang <jackchang0721@gmail.com>
2015-01-26 20:40:52 +0800

    Jack-20150126-Add DBFactory.cs

---
+++ MyLibrary/BaseCode/ADO/Repository/DBFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary.BaseCode.ADO.InterFace;
+
+namespace MyLibrary.BaseCode.ADO.Repository
+{
+    public static class DBFactory
+    {
+        public static IDBProvider GetDBFactory(CustomDBType DBType)
+        {
+
+            IDBProvider temp = null;
+            switch (DBType)
+            {
+                case CustomDBType.MsSQL:
+                    temp = new CustomMsSQL();
+                    break;
+                case CustomDBType.MySql:
+                    temp = new CustomMySql();
+                    break;
+                default:
+                    break;
+            }
+
+            return temp;
+        }
+
+    }
+
+
+    public enum CustomDBType
+    {
+        MsSQL = 0, MySql = 1
+    }
+}
