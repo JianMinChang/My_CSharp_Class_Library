9037c56455b42e610a44a2d549ba30089bad9315
JianMinChang <jackchang0721@gmail.com>
2015-01-29 20:17:30 +0800

    Jack-20150129-Modify OSDetail.cs

--- MyLibrary/Mobile/OSDetail.cs
+++ MyLibrary/Mobile/OSDetail.cs
@@ -7,14 +7,19 @@ using System.Text.RegularExpressions;
 
 namespace MyLibrary.Mobile
 {
-    public class OSDetail
+    public static class OSDetail
     {
-
-        private static string IOSRegex = @"iPhone OS [0-9]_[0-9]_[0-9]|iPhone OS [0-9]_[0-9]";
+        private static string IOSRegex = @"iPhone OS [0-9]_[0-9]_[0-9]|iPhone OS [0-9]_[0-9]|iPad; CPU OS [0-9]_[0-9]_[0-9]|iPad; CPU OS [0-9]_[0-9]|iPad;U;CPU OS [0-9]_[0-9]_[0-9]|iPad;U;CPU OS [0-9]_[0-9]|iPad; U; CPU OS [0-9]_[0-9]_[0-9]|iPad; U; CPU OS [0-9]_[0-9]";
         private static string AndroidRegex = @"Android [0-9].[0-9].[0-9]|Android [0-9].[0-9]|Android[0-9].[0-9].[0-9]|Android[0-9].[0-9]";
         private static string WindowPhoneRegex = @"Windows Phone [0-9].[0-9]";
+        private static string DeviceRegex = @"mobile|Mobile|iPad|IPAD";
 
-        private string GetOSString(string input)
+        private static string iOSosRegex = @"iPhone OS|iPhone OS|iPad; CPU OS|iPad; CPU OS|iPad;U;CPU OS|iPad;U;CPU OS|iPad; U; CPU OS|iPad; U; CPU OS";
+        private static string AndroidosRegex = @"Android";
+        private static string WindowPhoneosRegex = @"Windows Phone";
+
+
+        private static string GetOSString(string input)
         {
             return Regex.Match(input, @"(" + IOSRegex.ToString() + "|" + AndroidRegex.ToString() + "|" + WindowPhoneRegex.ToString() + ")").Value;
         }
@@ -24,13 +29,13 @@ namespace MyLibrary.Mobile
         /// </summary>
         /// <param name="UserAgent">UserAgent</param>
         /// <returns>作業系統名稱</returns>
-        public string GetMobileOS(string UserAgent)
+        public static string GetMobileOS(string UserAgent)
         {
             string temp = GetOSString(UserAgent);
 
             if (!string.IsNullOrEmpty(temp))
             {
-                return Regex.Match(temp, @"(iPhone OS|Android|Windows Phone)").Value.Replace(" OS", string.Empty);
+                return Regex.Replace(Regex.Match(temp, @"(" + iOSosRegex + "|" + AndroidosRegex + "|" + WindowPhoneosRegex + ")").Value, iOSosRegex, "iOS");
             }
             else
             {
@@ -43,7 +48,7 @@ namespace MyLibrary.Mobile
         /// </summary>
         /// <param name="UserAgent">UserAgent</param>
         /// <returns>版本號</returns>
-        public string GetMobileOSVersion(string UserAgent)
+        public static string GetMobileOSVersion(string UserAgent)
         {
             string temp = GetOSString(UserAgent);
 
@@ -57,6 +62,38 @@ namespace MyLibrary.Mobile
             }
         }
 
+        /// <summary>
+        /// 識別Device是(iPad or Mobile)
+        /// </summary>
+        /// <param name="UserAgent">UserAgent</param>
+        /// <returns>iPad or Mobile</returns>
+        public static string GetDeviceStatus(string UserAgent)
+        {
+            string temp = GetOSString(UserAgent);
 
+            if (!string.IsNullOrEmpty(temp))
+            {
+                if (Regex.Match(temp, DeviceRegex).Success)
+                {
+                    return Regex.Match(temp, DeviceRegex).Value;
+                }
+                else
+                {
+                    if (Regex.Match(UserAgent, DeviceRegex).Success)
+                    {
+                        return Regex.Match(UserAgent, DeviceRegex).Value;
+                    }
+                    else
+                    {
+                        return "iPad";
+                    }
+
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
     }
 }
