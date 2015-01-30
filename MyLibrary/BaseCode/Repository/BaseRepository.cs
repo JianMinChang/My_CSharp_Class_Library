327f7060c4e639e257546169a2cf96c8de1fd7a0
JianMinChang <jackchang0721@gmail.com>
2015-01-30 19:52:01 +0800

    Jack-20150130-Add BaseRepository.cs

---
+++ MyLibrary/BaseCode/Repository/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MyLibrary.BaseCode.InterFace;
+using MyLibrary.BaseCode.ADO.InterFace;
+
+namespace MyLibrary.BaseCode.Repository
+{
+    public class BaseRepository : IBaseRepository
+    {
+        public bool IsSuccess { protected set; get; }
+
+        public string ErrorMessage { protected set; get; }
+
+        protected IList<IDataParameter> Parms = new List<IDataParameter>();
+        protected DataTable dt = new DataTable();
+
+        /// <summary>
+        /// 使用前，屬性回復預設值
+        /// </summary>
+        protected void BeforeUseReset()
+        {
+            this.IsSuccess = false;
+            this.ErrorMessage = string.Empty;
+            this.dt = new DataTable();
+            if (this.Parms == null)
+            {
+                this.Parms = new List<IDataParameter>();
+            }
+            else
+            {
+                this.Parms.Clear();
+            }
+        }
+
+        /// <summary>
+        /// 使用完，屬性回復預設值
+        /// </summary>
+        protected void AfterUseReset()
+        {
+            this.Parms.Clear();
+        }
+
+
+
+        protected IDBProvider DBProvider { set; get; }
+
+    }
+}
