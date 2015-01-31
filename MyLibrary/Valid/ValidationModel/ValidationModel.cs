c076f8e38fbe64d09a6321d78bb4c12c4c5faf90
JianMinChang <jackchang0721@gmail.com>
2015-01-31 16:44:42 +0800

    Jack-20150131-Add CustomValidObj

---
+++ MyLibrary/Valid/ValidationModel/ValidationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MyLibrary.Valid.ValidationModel
+{
+    public static class ValidationModel
+    {
+        public static ValidStatusResult Validate<T>(T model)
+        {
+            var context = new ValidationContext(model, serviceProvider: null, items: null);
+            IList<ValidationResult> validationResults = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(model, context, validationResults, validateAllProperties: true);
+
+            return new ValidStatusResult() { IsSuccess = result, ErrorMessage = validationResults };
+        }
+    }
+
+
+    public class ValidStatusResult
+    {
+        public bool IsSuccess { set; get; }
+        public IList<ValidationResult> ErrorMessage { set; get; }
+    }
+}
