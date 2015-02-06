999649c5f84adc652612f11da72fa1d4628ebbc8
JianMinChang <jackchang0721@gmail.com>
2015-02-06 20:17:11 +0800

    Jack-20150206-Add RegexFormReposityAttribute.cs

---
+++ MyLibrary/Attribute/Web/RegexFormetReporsityAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Attribute
+{
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class RegexFormetReporsityAttribute : System.Attribute
+    {
+        public string RegexStr { get; set; }
+
+        public RegexFormetReporsityAttribute(string RegexStr)
+        {
+            this.RegexStr = RegexStr;
+        }
+
+        public override string ToString()
+        {
+            return this.RegexStr.ToString();
+        }
+    }
+}
