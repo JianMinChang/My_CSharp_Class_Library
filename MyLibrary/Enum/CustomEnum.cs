8d59645e83329c90e230bb183601e8b00a44453b
JianMinChang <jackchang0721@gmail.com>
2015-02-07 13:46:33 +0800

    Jack-20150207-Add CustomEnum.cs

---
+++ MyLibrary/Enum/CustomEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Enum
+{
+    public static class CustomEnum
+    {
+        public static string GetEnumStrAttribute<T, U>(T EnumType)
+            where T : struct
+            where U : class
+        {
+            var members = typeof(T).GetMember(EnumType.ToString());
+            var attributes = members[0].GetCustomAttributes(typeof(U), false);
+            var description = ((U)attributes[0]).ToString();
+            return description;
+        }
+    }
+}
