c853e9933aa3005f2b46fdb08a1414561bc1b13e
JianMinChang <jackchang0721@gmail.com>
2015-02-13 22:59:01 +0800

    Jack-20150213 Add UseSSLAttributes 註解

--- MyLibrary/Attribute/General/UseSSLAttribute.cs
+++ MyLibrary/Attribute/General/UseSSLAttribute.cs
@@ -10,8 +10,15 @@ using System.Web.Http.Filters;
 
 namespace MyLibrary.Attribute.Web
 {
+    /// <summary>
+    /// 自訂使用者屬性，判斷是否有Https
+    /// </summary>
     public class UseSSLAttribute : AuthorizationFilterAttribute
     {
+        /// <summary>
+        /// 重寫驗證判斷
+        /// </summary>
+        /// <param name="actionContext"></param>
         public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
             if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
